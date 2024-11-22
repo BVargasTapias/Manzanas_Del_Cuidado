0a6aa63419cb168037a6022465879dc4dfbc2fa7
BVargasTapias <brayanvargas0710@gmail.com>
2024-11-22 11:04:39 -0500

    Se actualiza api, base de datos y front

--- API/app.js
+++ API/app.js
@@ -83,9 +83,25 @@ app.post('/sesion', async (req, res) => {
       req.session.usuario = datos[0].Usu_NombreCompleto
       req.session.Documento = documento
       const usuario = { nombre: datos[0].Usu_NombreCompleto }
+      const rolUsuario = datos[0].Usu_TipoUsuario
       res.locals.usuario = usuario
       res.locals.Documento = documento
-      res.sendFile(path.join(__dirname, '../front/usuario.html'))
+      switch (rolUsuario) {
+        case "Administrador":
+          res.sendFile(path.join(__dirname, '../front/administrador.html'))
+          break;
+        case "Usuario":
+          res.sendFile(path.join(__dirname, '../front/usuario.html'))
+          break;
+
+        case null:
+
+          res.sendFile(path.join(__dirname, '../front/usuario.html'))
+          break;
+          default:
+            break;
+        }
+
       await conect.end()
     }
     else {
@@ -120,6 +136,7 @@ app.get('/obtener-usuario', (req, res) => {
 
 app.post('/obtener-servicios-usuario', async (req, res) => {
   const usuario = req.session.usuario
+  console.log(req.session.usuario);
   try {
     const conect = await mysql2.createConnection(db)
     const [datos] = await conect.execute('SELECT servicios.Ser_Nombre FROM servicios INNER JOIN servicios_manzanas ON servicios_manzanas.FK_ID_Servicios=servicios.ID_Servicios INNER JOIN manzanas ON manzanas.ID_Manzanas=servicios_manzanas.FK_ID_Manzanas INNER JOIN usuario ON manzanas.ID_Manzanas=usuario.FK_ID_Manzanas WHERE usuario.Usu_NombreCompleto=?', [usuario])
@@ -165,14 +182,13 @@ app.post('/obtener-servicios-guardados', async (req, res) => {
     // Conectar a la base de datos y consultar los servicios guardados por el usuario
     const conect = await mysql2.createConnection(db)
     const [IDU] = await conect.execute('SELECT usuario.ID_Usuario FROM usuario WHERE usuario.Usu_NumeroDocumento=?', [Documento])
-    const [serviciosGuardadosData] = await conect.execute('SELECT servicios.Ser_Nombre, solicitudes.Sol_Fechahora, solicitudes.FK_ID_Solicitudes FROM servicios INNER JOIN servicios_manzanas ON servicios_manzanas.FK_ID_Servicios=servicios.ID_Servicios INNER JOIN manzanas ON servicios_manzanas.FK_ID_Manzanas=manzanas.ID_Manzanas INNER JOIN usuario ON manzanas.ID_Manzanas=usuario.FK_ID_Manzanas INNWE JOIN solicitudes ON usuario.Id=solicitudes.ID1 WHERE solicitudes.ID1=? AND servicios.ID_Servicios=solicitudes.Sol_Codigoserv;', [IDU[0].Id]);
+    const [serviciosGuardadosData] = await conect.execute('SELECT servicios.Ser_Nombre, solicitudes.Sol_FechaHora, solicitudes.ID_Solicitudes FROM servicios INNER JOIN servicios_manzanas ON servicios_manzanas.FK_ID_Servicios=servicios.ID_Servicios INNER JOIN manzanas ON servicios_manzanas.FK_ID_Manzanas=manzanas.ID_Manzanas INNER JOIN usuario ON manzanas.ID_Manzanas=usuario.FK_ID_Manzanas INNER JOIN solicitudes ON usuario.ID_Usuario=solicitudes.FK_ID_Usuario WHERE solicitudes.FK_ID_Usuario=? AND servicios.ID_Servicios=solicitudes.Sol_Codigoserv;', [IDU[0].ID_Usuario]);
 
     console.log(serviciosGuardadosData)
-    console.log(IDU[0].Id)
     const serviciosGuardadosFiltrados = serviciosGuardadosData.map(servicio => ({
-      Nombre: servicio.Nombre,
-      Fecha: servicio.Fecha,
-      id: servicio.Id_solicitudes
+      Nombre: servicio.Ser_Nombre,
+      Fecha: servicio.Sol_FechaHora,
+      id: servicio.ID_Solicitudes 
       //Agrega más propiedades si es necesario
     }));
     //Enviar los datos de los servidores guardados al cliente
@@ -187,18 +203,47 @@ app.post('/obtener-servicios-guardados', async (req, res) => {
 
 });
 
-app.delete('/eliminar-servicio/:id', async (req, res) => {
-  const servicioId = req.params.id;
-  const eliminarServicios=db[Id];
-
-  if (!account){
-    return res.status(404).json({error: "Servicio no Eliminado"});
+app.post('/manzanas', async (req, res) => {
+  try {
+    const conect = await mysql2.createConnection(db);
+    const [tiposServicios] = await conect.execute('SELECT ID_Manzanas, Man_Nombre FROM manzanas');
+    res.json(tiposServicios);
+    console.log(tiposServicios);
+    await conect.end();
+  } catch (error) {
+    console.error('Error al obtener tipos de servicios:', error);
+    res.status(500).send('Error al obtener tipos de servicios');
   }
-delete db [Id];
-return res.status(204).send('Servicios eliminado');
+});
+
+app.post('/cerrar-sesion', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error al cerrar sesión:', err);
+      res.status(500).send('Error al cerrar sesión');
+    } else {
+      res.status(200).send('Sesión cerrada con éxito');
+    }
+  });
+});
+
+app.delete('/eliminar-servicio/:id', async (req, res) => {
+  const solicitudID = req.params.id; 
+  console.log("Solicitud: ", solicitudID);
+
+  try {
+    const conect = await mysql2.createConnection(db)
+    await conect.execute('DELETE FROM Solicitudes WHERE ID_Solicitudes =?', [solicitudID])
+    res.send().status(200)
+    await conect.end();
+
+  } catch (error) {
+    console.error('Error al eliminar la solicitud:', error);
+    res.status(500).send('Error al eliminar la solicitud');
+  }
 })
 
-
+ 
 //Apertura del servidor
 app.listen(3000, () => {
   console.log(`Servidor Node.js escuchando`);
